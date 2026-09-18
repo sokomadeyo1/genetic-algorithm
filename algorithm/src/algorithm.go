92c48cc7146a0a53cc948ee7ca7b06f0de7900db
Yuri Tihonov <sokomadeyo1@proton.me>
2026-09-18 23:50:51 +0300

    implement mutation

--- algorithm/src/algorithm.go
+++ algorithm/src/algorithm.go
@@ -154,7 +154,19 @@ func sampleIndex(dist []float64) int {
 }
 
 // Random swap
-func Mutation(species Species) Species { return nil }
+func Mutation(species Species) Species {
+	n := len(species)
+	i := rand.Intn(n)
+	j := rand.Intn(n)
+	for j == i {
+		j = rand.Intn(n)
+	}
+
+	species[i].Finish, species[j].Finish = species[j].Finish, species[i].Finish
+	species[(i+1)%n].Start, species[(j+1)%n].Start = species[(j+1)%n].Start, species[(i+1)%n].Start
+
+	return species
+}
 
 // Unite solutions and generate a new one on a resulting graph
 func Crossover(species []Species) Species { return nil }
