192a7f961336f01ad0cc9aef89ae92393c6af748
sokomadeyo1 <sokomadeyo1@proton.me>
2026-09-20 15:43:20 +0300

    Optimize visualisation (#1)
    * shorten max amount of frames
    
    * draw frames lazily for more effective memory utilization
    
    * update gitignore
    
    * update requirements.txt
    
    add tqdm

--- visuals/main.py
+++ visuals/main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 import sys
-from math import cos, pi, sin
+from math import ceil, cos, pi, sin
 
 import yaml
 from PIL import Image, ImageDraw
+from tqdm import tqdm
 
 USAGE = f"Usage: {sys.argv[0]} data.yaml output.gif"
 SIZE = (1600, 1600)
@@ -11,6 +12,7 @@ CENTER = (800, 800)
 RADIUS = 600
 STROKE = 2
 TOTAL_DURATION = 5000
+TOTAL_FRAMES = 250
 
 
 def draw_generation(draw: ImageDraw.ImageDraw, gen: list[list[int]], n: int):
@@ -26,6 +28,7 @@ def draw_generation(draw: ImageDraw.ImageDraw, gen: list[list[int]], n: int):
 
 
 def draw_path(draw: ImageDraw.ImageDraw, points: list[int], n: int):
+    """Get frames lazily."""
     for p1, p2 in zip(points, points[1:]):
         draw_edge(draw, p1, p2, n)
 
@@ -40,6 +43,14 @@ def draw_edge(draw: ImageDraw.ImageDraw, start: int, finish: int, n: int, color)
     draw.line([p1, p2], fill=color, width=STROKE)
 
 
+def gen_frames(data: list[list[list[int]]], n: int):
+    for gen in tqdm(data):
+        with Image.new("RGB", SIZE, "white") as img:
+            draw = ImageDraw.Draw(img)
+            draw_generation(draw, gen, n)
+            yield img
+
+
 def main():
     if len(sys.argv) < 3:
         print(USAGE)
@@ -48,16 +59,14 @@ def main():
     with open(fi, "r") as f:
         data = yaml.safe_load(f)
     n = data["city_count"]
-    frames = []
-    for gen in data["simulation"]:
-        with Image.new("RGB", SIZE, "white") as img:
-            draw = ImageDraw.Draw(img)
-            draw_generation(draw, gen, n)
-            frames.append(img)
+    sim = data["simulation"]
+    # shorten data for faster gif generation
+    sim = sim[::ceil(len(sim)/TOTAL_FRAMES)]
 
-    duration = int(TOTAL_DURATION / len(frames))
-    frames[0].save(
-        sys.argv[2], append_images=frames[1:], duration=duration, loop=0
+    duration = int(TOTAL_DURATION / len(sim))
+    frames = gen_frames(sim, n)
+    next(frames).save(
+        sys.argv[2], append_images=frames, duration=duration, loop=0
     )
 
 
